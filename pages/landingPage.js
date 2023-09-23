6fb078cc1f53425ec7764aff15e9d9d44fdd45ee
IanCVal <86568417+IanCVal94@users.noreply.github.com>
2023-09-22 22:15:11 -0400

    changed so now it is running with some more detail and style

--- pages/landingPage.js
+++ pages/landingPage.js
@@ -1,6 +1,6 @@
-import { Heading } from '@chakra-ui/react';
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
+import { Button, ButtonGroup } from '@chakra-ui/react'
   
 export const getServerSideProps = async () => {
     try {
@@ -36,26 +36,80 @@ return (
       </Head>
       <main>
         <h1 className="title">
-          Georgia Tech Club Explorer
+          Georgia Tech<br></br>
+          Club Explorer
         </h1>
-        
-        <img className="landingImage" width="400" height="400" src="/img/Georgia-Tech-Yellow-Jackets-Logo.png" />
+        <div>
+          <Button  size='md' height='48px' width='200px' border='2px' color="yellow.500" textColor="white" background="yellow.500"
+          _hover={{
+            borderColor:"yellow.500",
+            textColor: "yellow.500",
+            background: "white",
+            textColor: "black",
+          }}>Explore Clubs</Button>
 
-        <h2 className="button">Explore Clubs</h2>
-        <h2 className="button">How it works</h2>
+          <Button size='md' height='48px' width='200px' border='2px' color="yellow.500" textColor="white" background="yellow.500"
+          _hover={{
+            borderColor:"yellow.500",
+            textColor: "yellow.500",
+            background: "white",
+            textColor: "black",
+          }}>How It Works</Button>
+          <img className="landingImage" width="400" height="400" src="/img/Georgia-Tech-Yellow-Jackets-Logo.png" />
+        </div>
+        <h2 className="secondaryTitle">
+          How It Works
+        </h2>
+        <p className='paragraph'>
+          This is how it works! Idk yet so this is just dummy text. I am going to smash my keyboard to test whether the text centers properly on the site with multiple sentences.<br></br><br></br> asfdhjkasdhkjfjahksdlhjkldafshjkldfsahjasdfhjkladfshjkljklahsdhbfasjivbnasljkegbfljksdhfaoisuwbfcdljsiahrgjlsadbfouaeuibcvhljdsadbgfosiadjbvlasjdhfsudlivblsuaecdbsvlusagheladvbaslufhblasjkdbvlaseuibvljsbvliasdj ;-P
+        </p>
       </main>
 
 
 
       <style jsx>{`
         .title {
-            margin: 0;
-            line-height: 1.15;
-            font-size: 4rem;
+          font-size: 80px;
+          display: flex;
+          align-items: center;
+          color: darkblue;
+          font-family: "din-2014",sans-serif;
+          font-weight: 600;
+          font-style: normal;
+          margin: 0;
+          text-transform: uppercase;
+          margin-top: 20px;
+          margin-bottom: 20px;
+          }
+        .secondaryTitle {
+            font-size: 50px;
+            color: #C29B0C;
+            font-family: "din-2014",sans-serif;
+            font-weight: 600;
+            font-style: normal;
+            text-transform: uppercase;
+            margin-top: 200px;
+            margin-bottom: 40px;
             text-align: center;
           }
+        .paragraph{
+          font-size: 20px;
+          color: darkblue;
+          font-family: "din-2014",sans-serif;
+          font-weight: 600;
+          font-style: normal;
+          margin-bottom: 40px;
+          text-align: center;
+          margin: 0 auto;
+          padding: 20px; /* Optional padding for spacing */
+          text-align: center; /* Center the text within the container */
+          margin: 0 auto; /* Center the container horizontally */
+          max-width: 700px;
+        }
         .landingImage{
-            text-align: right;
+          position: absolute;
+          top: 0;
+          right: 0;
         }
         
 `     }</style>
