c7c1a3f24a25faa67a2b49ec2cc4290786ac8e47
Ganning Xu <ganningxu@gmail.com>
2023-09-28 13:28:30 -0400

    fixed how it works section and simplegrid

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,30 @@
-import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
-import NavBar from "../components/NavBar"
-import { Button, Center, Box, Image, Flex, Badge, Text, ButtonGroup, Heading, Container, SimpleGrid, HStack } from '@chakra-ui/react'
+import Head from 'next/head';
+import clientPromise from '../lib/mongodb';
+import NavBar from "../components/NavBar";
+import { Button, Center, Box, Image, Flex, Badge, Text, ButtonGroup, Heading, Container, SimpleGrid, HStack } from '@chakra-ui/react';
 
-export const getServerSideProps = async () => {
-    try {
-      await clientPromise
-      return {
-        props: { isConnected: true },
-      }
-    } catch (e) {
-      console.error(e)
-      return {
-        props: { isConnected: false },
-      }
-    }
+export const getServerSideProps = async () =>
+{
+  try
+  {
+    await clientPromise;
+    return {
+      props: { isConnected: true },
+    };
+  } catch (e)
+  {
+    console.error(e);
+    return {
+      props: { isConnected: false },
+    };
   }
-  
+};
+
 export default function Home({
-    isConnected,
-  }) {
-return (
+  isConnected,
+})
+{
+  return (
     <div className="container">
       <Head>
         <title>GT Club Explorer Landing Page</title>
@@ -31,46 +35,51 @@ return (
 
       <Container maxW='container.lg' mx="auto" spacing={16}>
 
-        <SimpleGrid columns={{base: 1, lg: 2}} alignItems='center' mt={8} textAlign={{base: "center", lg: "left"}}>
+        <SimpleGrid columns={{ base: 1, lg: 2 }} alignItems='center' mt={8} textAlign={{ base: "center", lg: "left" }}>
           <Box>
-            <Heading fontSize={{base: '5xl', lg: '7xl'}} fontWeight='extrabold'>Georgia Tech <br/> <Text as='span' color='#16425B'>Club Explorer</Text></Heading>
+            <Heading fontSize={{ base: '5xl', lg: '7xl' }} fontWeight='extrabold'>Georgia Tech <br /> <Text as='span' color='#16425B'>Club Explorer</Text></Heading>
 
-            <HStack justify={{base: "center", lg: "flex-start"}}>
+            <HStack justify={{ base: "center", lg: "flex-start" }}>
               <Button border='2px' bg='#16425B' color="white" mt={4} mb={4}
-              _hover={{
-                borderColor:"yellow.500",
-                textColor: "yellow.500",
-                background: "white",
-                textColor: "black",
-              }}>Explore Clubs</Button>
+                _hover={{
+                  borderColor: "yellow.500",
+                  textColor: "yellow.500",
+                  background: "white",
+                  textColor: "black",
+                }}>Explore Clubs</Button>
 
-              <Button bg="" textColor="#16425B"  mt={4} mb={4}
-              _hover={{
-                borderColor:"yellow.500",
-                textColor: "yellow.500",
-                background: "white",
-                textColor: "black",
-              }}>How It Works</Button>
+              <Button bg="" textColor="#16425B" mt={4} mb={4}
+                _hover={{
+                  borderColor: "yellow.500",
+                  textColor: "yellow.500",
+                  background: "white",
+                  textColor: "black",
+                }}>How It Works</Button>
             </HStack>
           </Box>
 
-          <Image mx="auto" display={{base: "none", lg: "block"}} src="/img/Georgia-Tech-Yellow-Jackets-Logo.png" maxH='250px' />
+          <Image mx="auto" display={{ base: "none", lg: "block" }} src="/img/Georgia-Tech-Yellow-Jackets-Logo.png" maxH='250px' />
 
         </SimpleGrid>
 
         {/* TODO:
             - how it works section
             - instead of using "Flex" for the section below, use "SimpleGrid"
-        
         */}
-        
 
-        <Flex
-            justifyContent="center"
-            alignItems="center"
-            flexWrap="wrap"
-            paddingBottom={40}
-        >
+        <Box textAlign="left" marginTop="128px">
+          <Heading>
+            How It Works
+          </Heading>
+          <Text>
+            This is how it works! Idk yet so this is just dummy text.
+            I am going to smash my keyboard to test whether the text centers properly on the site with multiple sentences.<br></br><br></br>
+            asfdhjkasdhkjfjahksdlhjkldafshjkldfsahjasdfhjkladfshjkljklahsdhbfasjivbnasljkegbfljksdhfaoisuwbfcdljsiahrgjlsadbfouaeuibcvhljdsadbgf
+            osiadjbvlasjdhfsudlivblsuaecdbsvlusagheladvbaslufhblasjkdbvlaseuibvljsbvliasdj ;-P
+          </Text>
+        </Box>
+
+        <SimpleGrid columns={{ base: 2, lg: 3 }}>
           {Array.from({ length: 3 }).map((_, index) => (
             <Box
               key={index}
@@ -78,10 +87,10 @@ return (
               maxW="320px"
               mx={4}
             >
-              <Center h="50vh" padding={1}>
+              <Center >
                 <Box p="5" maxW="320px" borderWidth="3px">
                   <Image borderRadius="md" src="https://www.wheeltest.com/cm/dpl/images/create/placeholder(1).jpg" />
-                  <Flex align="baseline" mt={2}>
+                  <Flex align="baseline" >
                     <Badge colorScheme="blue">Tags</Badge>
                     <Text
                       textTransform="uppercase"
@@ -100,7 +109,8 @@ return (
               </Center>
             </Box>
           ))}
-        </Flex>
+        </SimpleGrid>
+
       </Container>
 
       {/* <main>
@@ -243,5 +253,5 @@ return (
       }</style> */}
 
     </div>
-    )
-  }
+  );
+}
