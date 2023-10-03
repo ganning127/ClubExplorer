122999ad1d5668fe7c30fd5ddb5904674ecfbfe8
Ganning Xu <ganningxu@gmail.com>
2023-10-03 19:26:43 -0400

    added explore page example code

--- pages/explore.js
+++ pages/explore.js
@@ -1,60 +1,178 @@
-import Head from 'next/head'
-import { Box, Flex, Input, Grid } from "@chakra-ui/react";
+// <<<<<<< HEAD
+// import Head from 'next/head'
+// import { Box, Flex, Input, Grid } from "@chakra-ui/react";
+// import Navbar from "../components/NavBar";
+// import Card from "../components/ExploreCard";
+
+
+
+// export default function Explore() {
+
+
+//   return (
+//     <>
+//       <Head>
+//         <title>GT Club Explorer - Find Clubs</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <Box>
+//       <Navbar />
+//       <Flex>
+
+//         {/* Left Sidebar */}
+//         <Box w="20%" p={4}>
+
+//           {/* Search Filters */}
+//           <Box mb={4}>
+//             <Input placeholder="Filter 1" />
+//           </Box>
+//           <Box mb={4}>
+//             <Input placeholder="Filter 2" />
+//           </Box>
+//           {/* Add more filters here */}
+//         </Box>
+
+//         {/* Main Content */}
+//         <Box w="80%" p={4}>
+//           {/* Search Bar */}
+//           <Box mb={4}>
+//             <Input placeholder="Search organizations" />
+//           </Box>
+
+//           {/* 2-Column Grid of Cards */}
+//           <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+//             <Card title="Organization 1" description="Description 1" />
+//             <Card title="Organization 2" description="Description 2" />
+//             <Card title="Organization 3" description="Description 3" />
+//             <Card title="Organization 4" description="Description 4" />
+//             <Card title="Organization 5" description="Description 5" />
+//             <Card title="Organization 6" description="Description 6" />
+//             {/* Add more cards here */}
+//           </Grid>
+//         </Box>
+//       </Flex>
+//     </Box>
+
+
+//     </>
+//   );
+// }
+
+// =======
+import Head from 'next/head';
+import clientPromise from '../lib/mongodb';
+import NavBar from "../components/NavBar";
+import { Button, Center, Box, Image, Flex, Badge, Text, Link, ButtonGroup, Heading, Container, SimpleGrid, HStack } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { Input, Grid } from "@chakra-ui/react";
 import Navbar from "../components/NavBar";
-import Card from "../components/ExploreCard";
+// import Card from "../components/ExploreCard";
+
+export default function Explore({ success, clubs })
+{
+
+    console.log(success);
+    console.log(clubs);
+
+    return (
+        <>
+            <Head>
+                <title>Explore | GT Club Explorer</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <NavBar />
+
+            {/* <Container maxW='container.xl' mx="auto" > */}
+            <Flex>
+                <Box w="20%" p={4}>
+
+                    {/* Search Filters */}
+                    <Box mb={4}>
+                        <Input placeholder="Filter 1" />
+                    </Box>
+                    <Box mb={4}>
+                        <Input placeholder="Filter 2" />
+                    </Box>
+                    {/* Add more filters here */}
+                </Box>
+
+                <Box w="80%" p={4}>
+                    {/* Search Bar */}
+                    <Box mb={4}>
+                        <Input placeholder="Search organizations" />
+                    </Box>
+                    <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} alignItems='center' mt={8} textAlign={{ base: "center", lg: "left" }} spacing={4}>
+                        {
+                            clubs.map((club, index) =>
+                            {
+                                return (
+                                    <Link href={`/club/${club.slug}`} _hover={{}} key={index}>
+                                        <Box p={4} shadow="xl" rounded="md" _hover={{
+                                            background: "gray.100",
+                                            transition: "all .2s"
+                                        }}>
+                                            <Heading fontSize="lg">
+                                                {club.name}
+                                            </Heading>
+
+                                            <HStack mt={2}>
+                                                {
+                                                    club.tags.map((tag, index) =>
+                                                    {
+                                                        return (
+                                                            <Badge key={index} colorScheme="blue">{tag}</Badge>
+                                                        );
+                                                    })
+                                                }
+                                            </HStack>
+
+                                            <Text fontSize='md' mt={4}>
+                                                {club.shortDesc}
+                                            </Text>
+                                        </Box>
+                                    </Link>
+                                );
+                            })
+                        }
+
+                    </SimpleGrid>
+                </Box>
+
+            </Flex>
 
 
-
-export default function Explore() {
-
-
-  return (
-    <>
-      <Head>
-        <title>GT Club Explorer - Find Clubs</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Box>
-      <Navbar />
-      <Flex>
-
-        {/* Left Sidebar */}
-        <Box w="20%" p={4}>
-
-          {/* Search Filters */}
-          <Box mb={4}>
-            <Input placeholder="Filter 1" />
-          </Box>
-          <Box mb={4}>
-            <Input placeholder="Filter 2" />
-          </Box>
-          {/* Add more filters here */}
-        </Box>
-
-        {/* Main Content */}
-        <Box w="80%" p={4}>
-          {/* Search Bar */}
-          <Box mb={4}>
-            <Input placeholder="Search organizations" />
-          </Box>
-
-          {/* 2-Column Grid of Cards */}
-          <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-            <Card title="Organization 1" description="Description 1" />
-            <Card title="Organization 2" description="Description 2" />
-            <Card title="Organization 3" description="Description 3" />
-            <Card title="Organization 4" description="Description 4" />
-            <Card title="Organization 5" description="Description 5" />
-            <Card title="Organization 6" description="Description 6" />
-            {/* Add more cards here */}
-          </Grid>
-        </Box>
-      </Flex>
-    </Box>
-
-
-    </>
-  );
+            {/* </Container> */}
+        </>
+    );
 }
 
+export async function getServerSideProps(context)
+{
+    try
+    {
+        const client = await clientPromise;
+        const db = client.db("clubs");
+        const collection = db.collection("clubsMain");
+
+        let clubs = await collection.find({}).toArray();
+        clubs = JSON.parse(JSON.stringify(clubs));
+
+        let objToReturn = {
+            props: {
+                success: true,
+                clubs: clubs
+            }
+        };
+        return objToReturn;
+
+    } catch (e)
+    {
+        console.error(e);
+        return {
+            props: { success: false },
+        };
+    }
+}
+// >>>>>>> 0e17922 (added explore page example code)
--- pages/index.js
+++ pages/index.js
@@ -1,109 +1,93 @@
-import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
-import NavBar from "../components/NavBar"
-import { Button, Center, Box, Image, Flex, Badge, Text, ButtonGroup, Heading, Container, SimpleGrid, HStack } from '@chakra-ui/react'
+import Head from 'next/head';
+import NavBar from "../components/NavBar";
+import { Button, Center, Box, Image, Flex, Badge, Text, ButtonGroup, Heading, Container, SimpleGrid, HStack } from '@chakra-ui/react';
 
-export const getServerSideProps = async () => {
-    try {
-      await clientPromise
-      return {
-        props: { isConnected: true },
-      }
-    } catch (e) {
-      console.error(e)
-      return {
-        props: { isConnected: false },
-      }
-    }
-  }
-  
-export default function Home({
-    isConnected,
-  }) {
-return (
-    <div className="container">
-      <Head>
-        <title>GT Club Explorer Landing Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home()
+{
+    return (
+        <div className="container">
+            <Head>
+                <title>GT Club Explorer Landing Page</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <NavBar />
+            <NavBar />
 
-      <Container maxW='container.lg' mx="auto" spacing={16}>
+            <Container maxW='container.lg' mx="auto" spacing={16}>
 
-        <SimpleGrid columns={{base: 1, lg: 2}} alignItems='center' mt={8} textAlign={{base: "center", lg: "left"}}>
-          <Box>
-            <Heading fontSize={{base: '5xl', lg: '7xl'}} fontWeight='extrabold'>Georgia Tech <br/> <Text as='span' color='#16425B'>Club Explorer</Text></Heading>
+                <SimpleGrid columns={{ base: 1, lg: 2 }} alignItems='center' mt={8} textAlign={{ base: "center", lg: "left" }}>
+                    <Box>
+                        <Heading fontSize={{ base: '5xl', lg: '7xl' }} fontWeight='extrabold'>Georgia Tech <br /> <Text as='span' color='#16425B'>Club Explorer</Text></Heading>
 
-            <HStack justify={{base: "center", lg: "flex-start"}}>
-              <Button border='2px' bg='#16425B' color="white" mt={4} mb={4}
-              _hover={{
-                borderColor:"yellow.500",
-                textColor: "yellow.500",
-                background: "white",
-                textColor: "black",
-              }}>Explore Clubs</Button>
+                        <HStack justify={{ base: "center", lg: "flex-start" }}>
+                            <Button border='2px' bg='#16425B' color="white" mt={4} mb={4}
+                                _hover={{
+                                    borderColor: "yellow.500",
+                                    textColor: "yellow.500",
+                                    background: "white",
+                                    textColor: "black",
+                                }}>Explore Clubs</Button>
 
-              <Button bg="" textColor="#16425B"  mt={4} mb={4}
-              _hover={{
-                borderColor:"yellow.500",
-                textColor: "yellow.500",
-                background: "white",
-                textColor: "black",
-              }}>How It Works</Button>
-            </HStack>
-          </Box>
+                            <Button bg="" textColor="#16425B" mt={4} mb={4}
+                                _hover={{
+                                    borderColor: "yellow.500",
+                                    textColor: "yellow.500",
+                                    background: "white",
+                                    textColor: "black",
+                                }}>How It Works</Button>
+                        </HStack>
+                    </Box>
 
-          <Image mx="auto" display={{base: "none", lg: "block"}} src="/img/Georgia-Tech-Yellow-Jackets-Logo.png" maxH='250px' />
+                    <Image mx="auto" display={{ base: "none", lg: "block" }} src="/img/Georgia-Tech-Yellow-Jackets-Logo.png" maxH='250px' />
 
-        </SimpleGrid>
+                </SimpleGrid>
 
-        {/* TODO:
+                {/* TODO:
             - how it works section
             - instead of using "Flex" for the section below, use "SimpleGrid"
         
         */}
-        
 
-        <Flex
-            justifyContent="center"
-            alignItems="center"
-            flexWrap="wrap"
-            paddingBottom={40}
-        >
-          {Array.from({ length: 3 }).map((_, index) => (
-            <Box
-              key={index}
-              p="5"
-              maxW="320px"
-              mx={4}
-            >
-              <Center h="50vh" padding={1}>
-                <Box p="5" maxW="320px" borderWidth="3px">
-                  <Image borderRadius="md" src="https://www.wheeltest.com/cm/dpl/images/create/placeholder(1).jpg" />
-                  <Flex align="baseline" mt={2}>
-                    <Badge colorScheme="blue">Tags</Badge>
-                    <Text
-                      textTransform="uppercase"
-                      fontSize="sm"
-                      paddingLeft={2}
-                      color="yellow.600"
-                    >
-                      Cool &bull; Goofy
-                    </Text>
-                  </Flex>
-                  <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
-                    Random Club
-                  </Text>
-                  <Text mt={2}>Description: yoooooooooooooooo waddup</Text>
-                </Box>
-              </Center>
-            </Box>
-          ))}
-        </Flex>
-      </Container>
 
-      {/* <main>
+                <Flex
+                    justifyContent="center"
+                    alignItems="center"
+                    flexWrap="wrap"
+                    paddingBottom={40}
+                >
+                    {Array.from({ length: 3 }).map((_, index) => (
+                        <Box
+                            key={index}
+                            p="5"
+                            maxW="320px"
+                            mx={4}
+                        >
+                            <Center h="50vh" padding={1}>
+                                <Box p="5" maxW="320px" borderWidth="3px">
+                                    <Image borderRadius="md" src="https://www.wheeltest.com/cm/dpl/images/create/placeholder(1).jpg" />
+                                    <Flex align="baseline" mt={2}>
+                                        <Badge colorScheme="blue">Tags</Badge>
+                                        <Text
+                                            textTransform="uppercase"
+                                            fontSize="sm"
+                                            paddingLeft={2}
+                                            color="yellow.600"
+                                        >
+                                            Cool &bull; Goofy
+                                        </Text>
+                                    </Flex>
+                                    <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+                                        Random Club
+                                    </Text>
+                                    <Text mt={2}>Description: yoooooooooooooooo waddup</Text>
+                                </Box>
+                            </Center>
+                        </Box>
+                    ))}
+                </Flex>
+            </Container>
+
+            {/* <main>
         <NavBar></NavBar>
         <h1 className="title">
           Georgia Tech<br></br>
@@ -179,69 +163,8 @@ return (
         </p>
       </main> */}
 
-      <style jsx>{`
-        .title {
-          font-size: 80px;
-          display: flex;
-          align-items: center;
-          color: darkblue;
-          font-family: "din-2014",sans-serif;
-          font-weight: 600;
-          font-style: normal;
-          margin: 0;
-          text-transform: uppercase;
-          margin-top: 20px;
-          margin-bottom: 20px;
-          }
-        .secondaryTitle {
-            font-size: 50px;
-            color: #C29B0C;
-            font-family: "din-2014",sans-serif;
-            font-weight: 600;
-            font-style: normal;
-            text-transform: uppercase;
-            margin-top: 200px;
-            margin-bottom: 40px;
-            text-align: center;
-          }
-        .paragraph{
-          font-size: 20px;
-          color: darkblue;
-          font-family: "din-2014",sans-serif;
-          font-weight: 600;
-          font-style: normal;
-          margin-bottom: 40px;
-          text-align: center;
-          margin: 0 auto;
-          padding: 20px; /* Optional padding for spacing */
-          text-align: center; /* Center the text within the container */
-          margin: 0 auto; /* Center the container horizontally */
-          max-width: 700px;
-          padding-bottom:40px;
-        }
-        .landingImage{
-          position: absolute;
-          top: 0;
-          right: 0;
-        }
-        .copyrightContainer {
-          font-size:2 0px;
-          box-sizing: border-box;
-          padding-right: 20px;
-          padding-left: 20px;
-          background-color: black;
-          text-align: center;
-          position: absolute;
-          bottom: 0;
-          width: 100%;
-          height: 30px;
-          color: white;
-        }
-`     }</style>
-      {/*<style jsx global>{
 
-      }</style> */}
+        </div>
+    );
+}
 
-    </div>
-    )
-  }
