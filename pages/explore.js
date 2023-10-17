fa4c71964bf9abc78c086dcf9db713202f0709b4
jiyaski <j.miyagi.hopkins@gmail.com>
2023-10-16 23:10:13 -0400

    improved searchbar appearance & refactored into SearchBar.jsx

--- pages/explore.js
+++ pages/explore.js
@@ -7,6 +7,7 @@ import { Input, Grid } from "@chakra-ui/react";
 import Navbar from "../components/NavBar";
 import Filters from "../components/Filters"; 
 import Card from "../components/ExploreCard"; 
+import SearchBar from "../components/SearchBar"; 
 
 
 {/* parameter `clubs` is an object containing the data for each club pulled from Mongo */} 
@@ -23,28 +24,34 @@ export default function Explore({ success, clubs })
 
             {/* main page layout */} 
             <Grid
-                columns={{ base: 1, md: 2, lg: 3, xl: 4 }} alignItems="center" mt={8} 
-                textAlign={{ base: "center", lg: "left" }} spacing={4} gridGap="40px"
-                w="90%" mx="auto" 
+                columns={{ base: 1, md: 2, lg: 3, xl: 4 }} 
+                gridGap={{ base: "20px", lg: "30px", xl: "40px" }} 
+                textAlign={{ base: "center", lg: "left" }} 
+                w={{ base: "100%", md: "95%", lg: "90%", xl: "85%"}} 
+                mx="auto" spacing={4} alignItems="center" mt={8} 
             >
 
                 {/* Filters */}
-                <Box gridColumn="1" gridRow="2" alignSelf="start" borderWidth={2}>
+                <Box gridColumn="1" gridRow="2" alignSelf="start">
                     <Filters />
                 </Box>
 
                     
                 {/* Search Bar */}
-                <Box gridColumn={{ base: 1, md: 2 }} gridRow="1">
-                    <Input placeholder="Search organizations" />
+                <Box 
+                    gridColumn={{ base: 1, md: 2 }} gridRow="1" 
+                    display="flex" justifyContent="center" alignItems="center"
+                >
+                    <SearchBar />
                 </Box>
 
 
                 {/* ExploreCards */} 
                 <SimpleGrid 
                     gridColumn={{ base: 1, md: 2 }} gridRow={{ base: 3, md: 2 }} 
-                    columns={{ base: 1, lg: 2, xl: 3 }} alignItems="center"
-                    textAlign={{ base: "center", lg: "left" }} spacing={4} 
+                    columns={{ base: 1, lg: 2, xl: 3 }} 
+                    textAlign={{ base: "center", lg: "left" }} 
+                    spacing={4} alignItems="center"
                     
                 >{
                     clubs.map((club, index) => {
