b8012fcd70883830971c15b9ae0a62866023d1c4
Ganning Xu <ganningxu@gmail.com>
2023-10-24 17:51:27 -0400

    added image to card

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,32 +1,37 @@
-import { Box, Link, Heading, HStack, Badge, Text } from "@chakra-ui/react";
+import { Box, Link, Heading, HStack, Badge, Text, Img } from "@chakra-ui/react";
 
 
-export default function ExploreCard({ club, index }) {
+export default function ExploreCard({ club, index })
+{
 
-  return (
-    <Link href={`/club/${club.slug}`} _hover={{}} key={index}>
-        <Box p={4} shadow="xl" rounded="md" _hover={{
-            background: "gray.100",
-            transition: "all .2s"
-        }}>
-            <Heading fontSize="lg">
-                {club.name}
-            </Heading>
+    return (
+        <Link href={`/club/${club.slug}`} _hover={{}} key={index}>
+            <Box p={4} border='1px solid lightgray' rounded="md" _hover={{
+                background: "gray.100",
+                transition: "all .2s"
+            }}>
 
-            <Text>{club._id}</Text>  {/* temporary, to distinguish cards since they all have same data */} 
+                <Img src={club.media.images[0]} rounded='md' />
+                <Heading fontSize="lg" mt={4}>
+                    {club.name}
+                </Heading>
 
-            <HStack mt={2} flexWrap="wrap" justifyContent={{base: "center", lg: "left"}}> {
-                club.tags.map((tag, index) => {
-                    return (
-                        <Badge key={index} colorScheme="blue">{tag}</Badge>
-                    );
-                })
-            }</HStack>
+                {/* <Text>{club._id}</Text>   */}
+                {/* temporary, to distinguish cards since they all have same data */}
 
-            <Text fontSize='md' mt={4}>
-                {club.shortDesc}
-            </Text>
-        </Box>
-    </Link>
-  )
+                <HStack mt={2} flexWrap="wrap" justifyContent={{ base: "center", lg: "left" }}> {
+                    club.tags.map((tag, index) =>
+                    {
+                        return (
+                            <Badge key={index} colorScheme="blue">{tag}</Badge>
+                        );
+                    })
+                }</HStack>
+
+                <Text fontSize='md' mt={4}>
+                    {club.shortDesc}
+                </Text>
+            </Box>
+        </Link>
+    );
 }
