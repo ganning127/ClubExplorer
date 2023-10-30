6160ed1b1362390bdc085b421b2f6709ee16c6a7
Ganning Xu <ganningxu@gmail.com>
2023-10-30 19:00:04 -0400

    works with raw postman data

--- pages/api/addImageToS3.js
+++ pages/api/addImageToS3.js
@@ -6,21 +6,27 @@ const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const s3region = process.env.S3_REGION;
 const s3bucket = process.env.S3_BUCKET;
 
-const s3upload = async (req) => {
-    console.log(req)
+export default async function s3upload(req, res)
+{
+    console.log(req);
     const s3 = new S3Client({
         region: s3region,
         credentials: {
-          accessKeyId: accessKey,
-          secretAccessKey: secretAccessKey,
+            accessKeyId: accessKey,
+            secretAccessKey: secretAccessKey,
         },
-      });
-    
+    });
+
+    // const key = `${Date.now().toString()}` + "-" + req.files.image.name;
+    const key = Date.now().toString();
+
+    const body = req.body;
+
     console.log("Connected");
     const params = {
         Bucket: s3bucket,
-        Key: `${Date.now().toString()}`+ "-" + req.files.image.name,
-        Body: req.files.image.data,
+        Key: key,
+        Body: body,
     };
     // sometimes wants req.files.file.data
 
@@ -29,14 +35,19 @@ const s3upload = async (req) => {
         params,
     });
 
-    upload.on('httpUploadProgress', (progress) => {
+    upload.on('httpUploadProgress', (progress) =>
+    {
         console.log(progress);
     });
-    console.log("here too")
-    const data = await upload.done().catch((err) => {
-        console.log("error here")
-        console.log(err)
-    });
-}
+    console.log("here too");
 
-module.exports = s3upload;
+    const data = await upload.done().catch((err) =>
+    {
+        console.log("error here");
+        console.log(err);
+    });
+
+    res.status(200).json({ data });
+
+
+}
