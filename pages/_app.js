b490edaaefe2ec080da61013e0af1874f1400b4b
IanCVal94 <86568417+IanCVal94@users.noreply.github.com>
2023-11-01 00:49:20 -0400

    testing with changes to page for sign in on splash page

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,13 @@ import { useColorMode } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { ClerkProvider } from '@clerk/nextjs';
 
+const publicPages = [
+  "/",
+  "/note/[slug]",
+  "/sign-in/[[...index]]",
+  "/sign-up/[[...index]]",
+];
+
 function ForceLightMode({ children }) {
   // force light mode b/c of ChakraUI bug
   const { colorMode, toggleColorMode } = useColorMode();
@@ -16,9 +23,11 @@ function ForceLightMode({ children }) {
   return children;
 }
 
+const isPublicPage = publicPages.includes(pathname);
 function MyApp({ Component, pageProps }) {
   return (
     <ClerkProvider {...pageProps}>
+      
       <ChakraProvider>
           <ForceLightMode>
         <Component {...pageProps} />
